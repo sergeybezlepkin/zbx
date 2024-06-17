94cf4e921753afb2b40f9c58cd71fe11906845ed
Sergey Bezlepkin <84637238+sergeybezlepkin@users.noreply.github.com>
2024-06-17 22:47:37 +0300

    Create install_docker_redhat.sh

---
+++ script/install_docker_redhat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dnf update
+dnf install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable docker.service
+systemctl start docker
+docker version
+docker compose version
+exit
